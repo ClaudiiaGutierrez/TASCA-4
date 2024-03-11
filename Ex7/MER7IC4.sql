cec50aa80e7b2609ef319640c695af2af3736331
ClaudiiaGutierrez <148210605+ClaudiiaGutierrez@users.noreply.github.com>
2024-03-11 11:19:28 +0100

    Add files via upload

---
+++ Ex7/MER7IC4.sql
@@ -0,0 +1,3 @@
+use MER7;
+INSERT INTO Linia_Ticket(ID, Num_factura, Num_Empresa, NIF, Domicili_Fiscal, Telefon, Web, Correu, Data_ticket, IVA, EMPLEAT_ID, CLIENT_ID, Forma_Pagamen)
+VALUES('24', '0009', '0005', '47543678A','Carrer La Pau','974834738','https://www.mercadona.es','mmercadona@gmail.com','2023-03-03', '10', '10', '5', 'tarjeta');
