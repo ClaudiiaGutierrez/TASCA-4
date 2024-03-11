8a84f4315ef9b253cd4a8599c95bd1b9c445b888
ClaudiiaGutierrez <148210605+ClaudiiaGutierrez@users.noreply.github.com>
2024-03-11 11:16:43 +0100

    Add files via upload

---
+++ Ex5/MER5IC2.sql
@@ -0,0 +1,3 @@
+use MER5;
+INSERT INTO CLIENT(DNI, Nom, Cognom, Dir, Municipi, Telefon, Email, User, PWD)
+VALUES('93845345S', 'Laura', 'Jimenez', 'Carrer Volta 3B', 'Madrid', '934753792', 'ljimenez@gmail.com', 'erh389');
---
+++ Ex5/MER5IC4.sql
@@ -0,0 +1,3 @@
+USE MER5;
+INSERT INTO CITA(ID, Data_cita, Lloc, CLIENT_VEHICLE_ID)
+VALUES('10', '2016-03-05', 'Carrer Es Ramal', '5');
